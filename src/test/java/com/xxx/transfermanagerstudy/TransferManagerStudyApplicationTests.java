e1bc7fd20e13013dcc2d3f9171ce8518585c807b
xiongxinxin <xxxionghenu@163.com>
2020-12-16 22:46:33 +0800

    初始化项目

---
+++ src/test/java/com/xxx/transfermanagerstudy/TransferManagerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxx.transfermanagerstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferManagerStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
